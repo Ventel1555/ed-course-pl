690c09bbc42b24410010ac807138998e5cf3efd7
Ventel1555 <ventelorp@gmail.com>
2025-06-30 23:16:25 +0300

    admin users and docker fix

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'email', 'role', 'is_active', 'is_superuser']
+    list_filter = ['role', 'is_active', 'is_superuser']
+    search_fields = ['username', 'email']
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'role', 'password1', 'password2'),
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)
